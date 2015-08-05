fb8d41fe06aafe232476ef2220379e579146e43d
joamag <joamag@gmail.com>
2015-08-05 15:52:43 +0100

    new copyright information

--- nature/src/pt/hive/eclipse/plugins/nature/HiveNatureAction.java
+++ nature/src/pt/hive/eclipse/plugins/nature/HiveNatureAction.java
@@ -1,5 +1,5 @@
 // Hive Development Framework
-// Copyright (C) 2008-2015 Hive Solutions Lda.
+// Copyright (c) 2008-2015 Hive Solutions Lda.
 //
 // This file is part of Hive Development Framework.
 //
